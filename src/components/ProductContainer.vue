c2184395d62c8393c132222759f7009b547f4ead
Phyllomedusa <matthew.vladimirovich@gmail.com>
2024-10-14 17:57:21 +0300

    Добавлена карусель для просмотра изображений и переименование параметров

--- src/components/ProductContainer.vue
+++ src/components/ProductContainer.vue
@@ -4,14 +4,23 @@
       <v-row>
         <v-col v-for="product in products" :key="product.name" cols="12" md="4">
           <v-card>
+            <v-carousel v-if="product.imageUrls && product.imageUrls.length > 0" hide-delimiters height="300">
+              <v-carousel-item v-for="(image, index) in product.imageUrls" :key="index">
+                <v-img :src="image" height="300px"></v-img>
+              </v-carousel-item>
+            </v-carousel>
+
             <v-card-title>{{ product.name }}</v-card-title>
-            <v-card-subtitle>{{ product.price }} ₽</v-card-subtitle>
+
             <v-card-text>{{ product.description }}</v-card-text>
+
             <v-list>
-              <v-list-item v-for="(value, key) in product.parameters" :key="key">
-                {{ key }}: {{ value }}
+              <v-list-item v-for="(value, key) in getRenamedParameters(product)" :key="key">
+                <span>{{ key }}: {{ value }}</span>
               </v-list-item>
             </v-list>
+
+            <v-card-subtitle class="price">{{ product.price }} ₽</v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
@@ -32,7 +41,13 @@ export default {
   },
   data() {
     return {
-      products: [],
+      products: [], // Инициализация массива продуктов
+      renamedParameters: {
+        model: 'Модель',
+        color: 'Цвет',
+        storageCapacity: 'Объем памяти',
+        // Добавьте любые другие параметры, которые необходимо переименовать
+      },
     };
   },
   watch: {
@@ -51,6 +66,15 @@ export default {
         console.error('Failed to fetch products:', error);
       }
     },
+    getRenamedParameters(product) {
+      // Метод принимает продукт как аргумент
+      const renamedParams = {};
+      for (const [key, value] of Object.entries(product.parameters)) {
+        const renamedKey = this.renamedParameters[key] || key;
+        renamedParams[renamedKey] = value;
+      }
+      return renamedParams;
+    },
   },
 };
 </script>
@@ -58,5 +82,15 @@ export default {
 <style scoped>
 .product-container {
   margin-top: 20px;
+}
+
+.v-card {
+  margin-bottom: 20px;
+}
+
+.price {
+  font-size: 24px; /* Сделаем цену больше */
+  font-weight: bold;
+  margin-top: 10px;
 }
 </style>
